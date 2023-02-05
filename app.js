cd837faa461417f49f9ab6a4ea30a9e01caa91e3
Kuu44 <kushal-shrestha@hotmail.com>
2023-02-05 21:42:13 +0545

    Canvas

--- app.js
+++ app.js
@@ -28,12 +28,12 @@
 /* globals window, document, console */
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+// import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+// import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import MathType from '@wiris/mathtype-ckeditor5/src/plugin';
-import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config';
+// import { CS_CONFIG } from '@ckeditor/ckeditor5-cloud-services/tests/_utils/cloud-services-config';
 
 import Drawpad from './drawpad_plugin/drawpad'
 
@@ -44,48 +44,48 @@ import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 import { toWidget } from '@ckeditor/ckeditor5-widget/src/utils';
 // import { resolve } from 'path';
 
-const DrawPadCommand = ( editor ) => {
-    const model = editor.model;
-    const doc = model.document;
+const DrawPadCommand = (editor) => {
+	const model = editor.model;
+	const doc = model.document;
 
-    const drawPad = model.createElement( 'drawPad' );
-    model.insertContent( drawPad, doc.selection );
+	const drawPad = model.createElement('drawPad');
+	model.insertContent(drawPad, doc.selection);
 
-    toWidget( drawPad, editor );
-    model.setSelection( drawPad, 'on' );
+	toWidget(drawPad, editor);
+	model.setSelection(drawPad, 'on');
 };
 
-const DrawPadView = ( editor ) => {
-    const canvas = document.createElement( 'canvas' );
-    canvas.setAttribute( 'width', '400' );
-    canvas.setAttribute( 'height', '400' );
-    canvas.setAttribute( 'style', 'border: 1px solid #ddd' );
+const DrawPadView = (editor) => {
+	const canvas = document.createElement('canvas');
+	canvas.setAttribute('width', '400');
+	canvas.setAttribute('height', '400');
+	canvas.setAttribute('style', 'border: 1px solid #ddd');
 
-    const ctx = canvas.getContext( '2d' );
-    ctx.fillStyle = '#fff';
-    ctx.fillRect( 0, 0, 400, 400 );
+	const ctx = canvas.getContext('2d');
+	ctx.fillStyle = '#fff';
+	ctx.fillRect(0, 0, 400, 400);
 
-    canvas.addEventListener( 'mousedown', ( e ) => {
-        ctx.beginPath();
-        ctx.moveTo( e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop );
-    } );
+	canvas.addEventListener('mousedown', (e) => {
+		ctx.beginPath();
+		ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+	});
 
-    canvas.addEventListener( 'mouseup', ( e ) => {
-        ctx.lineTo( e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop );
-        ctx.stroke();
-    } );
+	canvas.addEventListener('mouseup', (e) => {
+		ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+		ctx.stroke();
+	});
 
-    return canvas;
+	return canvas;
 };
 
 
 ClassicEditor
-	.create( document.querySelector( '#mathtype-editor' ), {
+	.create(document.querySelector('#mathtype-editor'), {
 		plugins: [
 			ArticlePluginSet,
 			EasyImage,
 			ImageUpload,
-			CloudServices,
+			// CloudServices,
 			MathType,
 			Drawpad
 		],
@@ -125,46 +125,46 @@ ClassicEditor
 			]
 		},
 		table: {
-			contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
+			contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
 		},
-		cloudServices: CS_CONFIG
-	} )
+		// cloudServices: CS_CONFIG
+	})
 	// .then((editor)=>{
 	// 	console.log("jhilimilli");
 	// 	return editor;
 	// })
-    // .then( ( editor ) => {
+	// .then( ( editor ) => {
 	// 	console.log(editor);
-    //     editor.ui.componentFactory.add( 'drawPad', ( ) => {
-    //         const button = new ButtonView( );
+	//     editor.ui.componentFactory.add( 'drawPad', ( ) => {
+	//         const button = new ButtonView( );
 
-    //         // button.set( {
-    //         //     label: 'Draw',
-    //         //     command: 'drawPad'
-    //         // } );
-			
-    //         button.label = 'drawpad2';
-    //         button.tooltip = true;
-    //         button.withText = true;
+	//         // button.set( {
+	//         //     label: 'Draw',
+	//         //     command: 'drawPad'
+	//         // } );
+
+	//         button.label = 'drawpad2';
+	//         button.tooltip = true;
+	//         button.withText = true;
 
 	// 		// this.listenTo( button, 'execute', () => {
-    //         //     DrawPadCommand( editor );				
-    //         // } );
+	//         //     DrawPadCommand( editor );				
+	//         // } );
 
-    //         button.on( 'execute', () => {
-    //             DrawPadCommand( editor );
-    //         } );
+	//         button.on( 'execute', () => {
+	//             DrawPadCommand( editor );
+	//         } );
 
-    //         return button;
-    //     } );
+	//         return button;
+	//     } );
 
-    //     // editor.ui.add( 'drawPad', {
-    //     //     view: DrawPadView,
-    //     //     position: 'top'
-    //     // } );
+	//     // editor.ui.add( 'drawPad', {
+	//     //     view: DrawPadView,
+	//     //     position: 'top'
+	//     // } );
 
-    //     editor.ui.addToolbar( DrawPadView );
-    // } )
+	//     editor.ui.addToolbar( DrawPadView );
+	// } )
 
 	// .then( editor => {
 	// 	window.editor = editor;
@@ -176,6 +176,6 @@ ClassicEditor
 	// 		editor
 	// 	} );
 	// } )
-	.catch( err => {
-		console.error( err.stack );
-	} );
+	.catch(err => {
+		console.error(err.stack);
+	});
--- drawpad_plugin/drawpadview.js
+++ drawpad_plugin/drawpadview.js
@@ -1,25 +1,25 @@
 import {
     View,
-    LabeledFieldView,				
+    LabeledFieldView,
     createLabeledInputText,
     ButtonView,
     submitHandler,
-    Template,			
-    } from '@ckeditor/ckeditor5-ui';
+    Template,
+} from '@ckeditor/ckeditor5-ui';
 
 import { KeystrokeHandler } from '@ckeditor/ckeditor5-utils';
 
 import { icons } from '@ckeditor/ckeditor5-core';
 
 export default class CanvasView extends View {
-    constructor( locale ) {
-        super( locale );
-        
+    constructor(locale) {
+        super(locale);
+
         this.keystrokes = new KeystrokeHandler();
-        
-        this.strInputView = this._createInput( 'Add text' );
-        
-        
+
+        this.strInputView = this._createInput('Add text');
+
+
         // Create the save and cancel buttons.
         this.saveButtonView = this._createButton(
             'Save', icons.check, 'ck-button-save'
@@ -27,56 +27,98 @@ export default class CanvasView extends View {
         // Set the type to 'submit', which will trigger
         // the submit event on entire form when clicked.
         this.saveButtonView.type = 'submit';
-        
+
         this.cancelButtonView = this._createButton(
             'Cancel', icons.cancel, 'ck-button-cancel'
         );
         // Delegate ButtonView#execute to FormView#cancel.
-        this.cancelButtonView.delegate( 'execute' ).to( this, 'cancel' );
-        
-        this.childViews = this.createCollection( [
+        this.cancelButtonView.delegate('execute').to(this, 'cancel');
+
+        this.childViews = this.createCollection([
             this.strInputView,
             this.saveButtonView,
             this.cancelButtonView,
-        ] );
-        this.setTemplate( {
+        ]);
+        this.setTemplate({
             tag: 'form',
             attributes: {
-                class: [ 'ck', 'ck-abbr-form' ],
+                class: ['ck', 'ck-abbr-form'],
                 tabindex: '-1'
             },
-            children:[
+            children: [
                 this.getHTML_Canvas(),
                 ...this.childViews,
                 get_test_element()
             ],
-        } );
+        });
     }
-    getHTML_Canvas(){
-        const canvas = document.createElement( 'canvas' );
-        canvas.setAttribute( 'width', '400' );
-        canvas.setAttribute( 'height', '400' );
-        canvas.setAttribute( 'style', 'border: 1px solid #ddd' );
+    getHTML_Canvas() {
+        const canvas = document.createElement('canvas');
+        canvas.setAttribute('width', '400');
+        canvas.setAttribute('height', '400');
+        canvas.setAttribute('style', 'border: 1px solid #ddd');
 
-        const ctx = canvas.getContext( '2d' );
-        ctx.fillStyle = '#fff';
-        ctx.fillRect( 0, 0, 400, 400 );
+        const ctx = canvas.getContext('2d');
 
-        canvas.addEventListener( 'mousedown', ( e ) => {
-            ctx.beginPath();
-            ctx.moveTo( e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop );
-        } );
+        const canvasOffsetX = canvas.offsetLeft;
+        const canvasOffsetY = canvas.offsetTop;
 
-        canvas.addEventListener( 'mouseup', ( e ) => {
-            ctx.lineTo( e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop );
+        canvas.width = window.innerWidth - canvasOffsetX;
+        canvas.height = window.innerHeight - canvasOffsetY;
+
+        let isPainting = false;
+        let lineWidth = 5;
+        let startX;
+        let startY;
+
+        // toolbar.addEventListener('click', e => {
+        //     // if (e.target.id === 'clear') {
+        //     //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //     // }
+        // });
+
+        // toolbar.addEventListener('change', e => {
+        //     if (e.target.id === 'stroke') {
+        //         ctx.strokeStyle = e.target.value;
+        //     }
+
+        //     if (e.target.id === 'lineWidth') {
+        //         lineWidth = e.target.value;
+        //     }
+
+        // });
+
+        const draw = (e) => {
+            if (!isPainting) {
+                return;
+            }
+
+            ctx.lineWidth = lineWidth;
+            ctx.lineCap = 'round';
+
+            ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
             ctx.stroke();
-        } );
+        }
+
+        canvas.addEventListener('mousedown', (e) => {
+            isPainting = true;
+            startX = e.clientX;
+            startY = e.clientY;
+        });
+
+        canvas.addEventListener('mouseup', e => {
+            isPainting = false;
+            ctx.stroke();
+            ctx.beginPath();
+        });
+
+        canvas.addEventListener('mousemove', draw);
 
         return canvas;
     }
-    get_test_element(){
-        const button = document.createElement( 'button' );
-        button.onclick=()=>console.log("Clicked")
+    get_test_element() {
+        const button = document.createElement('button');
+        button.onclick = () => console.log("Clicked")
         return button;
     }
     render() {
@@ -84,9 +126,9 @@ export default class CanvasView extends View {
 
         // Submit the form when the user clicked the save button
         // or pressed enter in the input.
-        submitHandler( {
+        submitHandler({
             view: this
-        } );
+        });
 
         this.keystrokes.listenTo(this.element);
     }
@@ -94,22 +136,22 @@ export default class CanvasView extends View {
         this.childViews.first.focus();
     }
 
-    _createInput( label ) {
-        const labeledInput = new LabeledFieldView( this.locale, createLabeledInputText );
+    _createInput(label) {
+        const labeledInput = new LabeledFieldView(this.locale, createLabeledInputText);
 
         labeledInput.label = label;
 
         return labeledInput;
     }
-    _createButton( label, icon, className ) {
+    _createButton(label, icon, className) {
         const button = new ButtonView();
 
-        button.set( {
+        button.set({
             label,
             icon,
             tooltip: true,
             class: className
-        } );
+        });
 
         return button;
     }
