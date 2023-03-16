a0113b44d498a44cd25752f6210f4af73f839da3
Orj <61639823+VanIro@users.noreply.github.com>
2023-03-16 15:29:01 +0545

    Update drawpad.js

--- drawpad_plugin/drawpad.js
+++ drawpad_plugin/drawpad.js
@@ -2,6 +2,7 @@ import DrawpadUI from './drawpadui';
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import DrawpadEditing from './drawpadediting';
 
+//combines all the plugin components
 export default class Drawpad extends Plugin {
     static get requires() {
         return [ DrawpadEditing , DrawpadUI ];
